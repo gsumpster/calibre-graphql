61fef69f4fcb8aaf2d32aedf40f1a12526762599
George Sumpster <gsumpster@gmail.com>
2017-06-06 21:54:42 -0700

    made into module

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-var express = require("express");
-var graphqlHTTP = require("express-graphql");
-var cors = require("cors");
-var path = require("path");
+import express from "express";
+import graphqlHTTP from "express-graphql";
 
 import schema from "./schema";
 
-var app = express();
-app.use(cors());
-app.use("/img", express.static(path.join(__dirname, 'calibre')));
-app.use("/graphql", graphqlHTTP({
-	schema: schema,
-	pretty: true,
-	graphiql: true,
-}));
-
-app.listen(3000);
+export default (app, options) => {
+	if (options.noCovers) {
+		app.use(`${options.prefix}/image`, express.static(options.filePath));
+	}
+	app.use(`${options.prefix}/graphql`, graphqlHTTP({
+		schema: schema,
+		graphiql: options.graphiql,
+	}));
+};
